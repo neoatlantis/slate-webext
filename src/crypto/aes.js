e52915243e598f466c754b2d165111eb8ffb16a7
NeoAtlantis <contact@chaobai.li>
2024-09-12 21:03:08 +0200

    a decrypt func with problem

--- src/crypto/aes.js
+++ src/crypto/aes.js
@@ -1,5 +1,7 @@
 const msgpack = require('@ygoe/msgpack');
+const _ = require("lodash");
 const subtle = window.crypto.subtle;
+const buffer = require("buffer");
 
 
 async function encrypt(key, data){
@@ -16,4 +18,29 @@ async function encrypt(key, data){
 	return msgpack.serialize([iv, encrypted]);
 }
 
-export { encrypt }
+async function decrypt(key, data){
+	let deserialized = msgpack.deserialize(data);
+	let iv = _.get(deserialized, 0),
+		encrypted = _.get(deserialized, 1);
+
+	const params = { name: 'AES-CBC', iv };
+	let decrypted = msgpack.deserialize(new Uint8Array(
+		await subtle.decrypt(params, key, encrypted)
+	));
+
+	let sha256_got = _.get(decrypted, 0),
+		plaintext = _.get(decrypted, 1);
+
+	let sha256_found = await subtle.digest('SHA-256', plaintext);
+
+	if(
+		buffer.Buffer.from(sha256_got).toString('hex') ==
+		buffer.Buffer.from(sha256_found).toString('hex')
+	){
+		// TODO time constant eq
+		return plaintext;
+	}
+	throw Error("Plaintext corrupted.");
+}
+
+export { encrypt, decrypt }
