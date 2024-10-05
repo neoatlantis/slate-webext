7ee589f2a57a5109ed14f1bdf83e516c47da89be
NeoAtlantis <contact@chaobai.li>
2024-10-05 22:51:49 +0800

    make dir

--- zip-sources.sh
+++ zip-sources.sh
@@ -5,5 +5,6 @@ PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 
 OUTPUT_FILE=web-ext-artifacts/neoatlantis_slate_browser_$PACKAGE_VERSION.src.zip
 
+mkdir -p web-ext-artifacts
 zip -r $OUTPUT_FILE \
 	./src ./package.json ./pnpm-lock.yaml ./README.md ./webpack.config.js
